72b2331a696321ca93728df68d58faef2024b4b3
katie-ws <89932020+katie-ws@users.noreply.github.com>
2022-02-04 12:16:28 +0000

    First commit

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Website Creation',
+            category:'Website',
+            image:'/images/web-development.jpg',
+            price:350,
+            countInStock: 10,
+            brand:'KWS',
+            rating:4.5,
+            numReviews:10,
+            description:'Personalised and branded website to suit your business needs'
+        },
+        {
+            _id:'2',
+            name:'Website Refresh',
+            category:'Website',
+            image:'/images/web-development.jpg',
+            price:100,
+            countInStock: 20,
+            brand:'KWS',
+            rating:5.0,
+            numReviews:12,
+            description:'Updating your existing website to add new features, services, or new branding'
+        },
+        {
+            _id:'3',
+            name:'Search Engine Optimisation Analysis',
+            category:'Website',
+            image:'/images/seo.jpg',
+            price:100,
+            countInStock: 0,
+            brand:'KWS',
+            rating:4.0,
+            numReviews:6,
+            description:'Analysis of your current position and detailed report of suggestions to imrpove your search results'
+        },
+        {
+            _id:'4',
+            name:'Branded Photography',
+            category:'Photography',
+            image:'/images/brand-photography.jpg',
+            price:300,
+            countInStock: 15,
+            brand:'KWS',
+            rating:5.0,
+            numReviews:2,
+            description:'Consultation of your photography needs, images taken, edited and returned for you to use in your marketing campaigns'
+        },
+        {
+            _id:'5',
+            name:'Content Creation',
+            category:'Content',
+            image:'/images/content-creation.jpg',
+            price:80,
+            countInStock: 5,
+            brand:'MWS',
+            rating:5.0,
+            numReviews:15,
+            description:'Create personalised content for your website, blog, or press releases in your brand\'s voice'
+        },
+        {
+            _id:'6',
+            name:'Full Website Package',
+            category:'Website',
+            image:'/images/website-package.jpg',
+            price:700,
+            countInStock: 12,
+            brand:'KWS & MWS',
+            rating:5.0,
+            numReviews:4,
+            description:'All aspects of your website will be looked after: the style, branding, content and imagery'
+        },
+    ],
+};
+
+export default data;
